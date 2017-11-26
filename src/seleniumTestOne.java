9256f8a3c8502fb29f7e86ab0975321759bfae40
marcovanwoudenberg <marco.vanwoudenberg@hotmail.com>
2017-11-26 19:53:15 +0100

    rt org.openqa.selenium.By; import org.openqa.selenium.WebElement; import org.openqa.selenium.firefox.FirefoxDriver;
    public class TestSelenium {
    			public static void main(String[] args){
    				FirefoxDriver driver=new FirefoxDriver();
    				driver.get("http://demo.guru99.com/");
    				WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
    				element.sendKeys("abc@gmail.com");
    
    			qibutton.click();
    		}
    }

--- src/seleniumTestOne.java
+++ src/seleniumTestOne.java
@@ -5,11 +5,11 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestSelenium {
     public static void main(String[] args){
-        ChromeDriver driver=new ChromeDriver();
+        FirefoxDriver driver=new FirefoxDriver();
         driver.get("http://demo.guru99.com/");
         WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
         element.sendKeys("abc@gmail.com");
