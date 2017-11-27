f7811f79f49f7e8b63fd43564e7cdbac3915089b
marcovanwoudenberg <marco.vanwoudenberg@hotmail.com>
2017-11-27 10:52:10 +0100

    Update Learn Java for Dummies

--- src/seleniumTestOne.java
+++ src/seleniumTestOne.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TestSelenium {
+class TestSelenium {
     public static void main(String[] args){
         FirefoxDriver driver=new FirefoxDriver();
         driver.get("http://demo.guru99.com/");
